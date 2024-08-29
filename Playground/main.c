91173e66ab71f064fd2297e1d165c0caf5ee6fea
Arthur Bacon <bacon247@gmail.com>
2024-08-29 09:11:40 -0400

    Initial Commit

---
+++ Playground/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Playground
+//
+//  Created by Arthur Bacon on 8/29/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
